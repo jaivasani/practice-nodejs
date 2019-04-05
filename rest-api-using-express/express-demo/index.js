af522a0dea446a8b2140939ff670305bb6e6bc60
Jai Vasani <jsvasani@gmail.com>
2019-04-05 16:01:49 -0500

    Added data validation on post request input using Joi package from npm.

--- rest-api-using-express/express-demo/index.js
+++ rest-api-using-express/express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -19,10 +20,17 @@ app.get('/api/courses', (req, res) => {
 
 // POST Request
 app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+
     // Input validation
-    if (!req.body.name || req.body.name.length < 3) {
+    if (result.error) {
         // 400 Bad Request
-        res.status(400).send('Name is required and should be atlest 3 characters.');
+        res.status(400).send(result.error.details[0].message);
         return;
     }
 

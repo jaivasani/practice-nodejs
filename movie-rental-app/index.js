65d43ea707bb8ddb232f5458e4d02476438be394
Jai Vasani <jsvasani@gmail.com>
2019-04-05 22:26:53 -0500

    Handled get request which returns all genre.

--- movie-rental-app/index.js
+++ movie-rental-app/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+const genre = [
+    {id: 1, name: 'Action'},
+    {id: 2, name: 'Drama'},
+    {id: 3, name: 'Comedy'}
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world');
+});
+
+app.get('/api/genre', (req, res) => {
+    res.send(genre);
 });
 
 // PORT

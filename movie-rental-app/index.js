16c67d7428876c4ac4b7bdd9a9181c5914dd96b1
Jai Vasani <jsvasani@gmail.com>
2019-04-05 23:04:53 -0500

    Handled post request with input data validation.

--- movie-rental-app/index.js
+++ movie-rental-app/index.js
@@ -1,5 +1,9 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+// To parse JSON object during the POST request
+app.use(express.json());
 
 const genres = [
     {id: 1, name: 'Action'},
@@ -7,6 +11,14 @@ const genres = [
     {id: 3, name: 'Comedy'}
 ];
 
+function genreValidation(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+}
+
+// GET Request
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
@@ -23,6 +35,27 @@ app.get('/api/genres/:id', (req, res) => {
     if (!genre) return res.status(404).send('The genre with the given ID is not found');
     
     // Genre is found
+    res.send(genre);
+});
+
+// POST Request
+app.post('/api/genres', (req, res) => {
+    // Validating the course
+    const { error } = genreValidation(req.body);
+
+    // If error then 400 - Bad Request
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Create new genre object
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+
+    // Added genre to the genres array
+    genres.push(genre);
+
+    // Return the newly added genre
     res.send(genre);
 });
 

5cf1418badef9e8ad8a0a1babdbfa847b4457158
Jai Vasani <jsvasani@gmail.com>
2019-04-06 17:32:49 -0500

    Added another async function to read github repositories of a user using github api.

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,12 +1,25 @@
 console.log('Before');
 getUser(1, (user) => {
-    console.log('User', user)
+    // Get repositories
+    getRepositories('jaivasani', (repositories) => {
+    console.log('Repos:', repositories);
+    });
 });
+
+
+
 console.log('After');
 
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database...');
         callback({id: id, githubUsername: 'jaivasani'});
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Using github api to read the repositories of the user', username);
+        callback(['repo1', 'repo2', 'repo3']);
     }, 2000);
 }

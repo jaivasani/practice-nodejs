62e67cf83d1f7c3bc5d46de17bed82a74f0fd87a
Jai Vasani <jsvasani@gmail.com>
2019-04-06 16:26:19 -0500

    Restructured the application. Moved all the genres routes to filed name genres. Moved the home route to a home file under routes.

--- movie-rental-app/index.js
+++ movie-rental-app/index.js
@@ -1,100 +1,10 @@
-const Joi = require('joi');
+const genres = require('./routes/genres');
+const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
-// To parse JSON object during the POST request
-app.use(express.json());
-
-const genres = [
-    {id: 1, name: 'Action'},
-    {id: 2, name: 'Drama'},
-    {id: 3, name: 'Comedy'}
-];
-
-function genreValidation(genre) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(genre, schema);
-}
-
-// GET Request
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
-
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-});
-
-app.get('/api/genres/:id', (req, res) => {
-    // Check if genre id is in array
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-    // Genre not found, error 404
-    if (!genre) return res.status(404).send('The genre with the given ID is not found');
-    
-    // Genre is found
-    res.send(genre);
-});
-
-// POST Request
-app.post('/api/genres', (req, res) => {
-    // Validating genre
-    const { error } = genreValidation(req.body);
-
-    // If error then 400 - Bad Request
-    if (error) return res.status(400).send(error.details[0].message);
-
-    // Create new genre object
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    };
-
-    // Added genre to the genres array
-    genres.push(genre);
-
-    // Return the newly added genre
-    res.send(genre);
-});
-
-// PUT request
-app.put('/api/genres/:id', (req, res) => {
-    // Check if genre id is in array
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-    // Genre not found, error 404
-    if (!genre) return res.status(404).send('The genre with the given ID is not found');
-
-    // Check if the new genre name is in valid format
-    const { error } = genreValidation(req.body);
-
-    // If error then 400 - Bad Request
-    if (error) return res.status(400).send(error.details[0].message);
-
-    // Genre name is valid so update genre name
-    genre.name = req.body.name;
-
-    // Return updated genre
-    res.send(genre);
-});
-
-// DELETE request
-app.delete('/api/genres/:id', (req, res) => {
-    // Check if genre id is in array
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-
-    // Genre not found, return error 404 - Not Found
-    if (!genre) return res.status(404).send('The genre with the given ID is not found');
-
-    // Delete genre
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    // Return deleted genre
-    res.send(genre);
-});
+app.use('/api/genres', genres);
+app.use('/', home);
 
 // PORT
 const port = process.env.PORT || 3000;
---
+++ movie-rental-app/routes/genres.js
@@ -0,0 +1,95 @@
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+// To parse JSON object during the POST request
+router.use(express.json());
+
+const genres = [
+    {id: 1, name: 'Action'},
+    {id: 2, name: 'Drama'},
+    {id: 3, name: 'Comedy'}
+];
+
+// GET Request
+router.get('/', (req, res) => {
+    res.send(genres);
+});
+
+router.get('/:id', (req, res) => {
+    // Check if genre id is in array
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+
+    // Genre not found, error 404
+    if (!genre) return res.status(404).send('The genre with the given ID is not found');
+    
+    // Genre is found
+    res.send(genre);
+});
+
+// POST Request
+router.post('/', (req, res) => {
+    // Validating genre
+    const { error } = genreValidation(req.body);
+
+    // If error then 400 - Bad Request
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Create new genre object
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+
+    // Added genre to the genres array
+    genres.push(genre);
+
+    // Return the newly added genre
+    res.send(genre);
+});
+
+// PUT request
+router.put('/:id', (req, res) => {
+    // Check if genre id is in array
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+
+    // Genre not found, error 404
+    if (!genre) return res.status(404).send('The genre with the given ID is not found');
+
+    // Check if the new genre name is in valid format
+    const { error } = genreValidation(req.body);
+
+    // If error then 400 - Bad Request
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Genre name is valid so update genre name
+    genre.name = req.body.name;
+
+    // Return updated genre
+    res.send(genre);
+});
+
+// DELETE request
+router.delete('/:id', (req, res) => {
+    // Check if genre id is in array
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+
+    // Genre not found, return error 404 - Not Found
+    if (!genre) return res.status(404).send('The genre with the given ID is not found');
+
+    // Delete genre
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    // Return deleted genre
+    res.send(genre);
+});
+
+function genreValidation(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+}
+
+module.exports = router;

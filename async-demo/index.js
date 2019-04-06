b973b891c36a599abed3d21f81c0014b415d1578
Jai Vasani <jsvasani@gmail.com>
2019-04-06 17:07:34 -0500

    Reading the return value from an async function.

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,14 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user from a database...');
-}, 2000);
+
+// User value is undefined because the value is not available yet
+const user = getUser(1); 
+
+console.log(user); // User value undefined
 console.log('After');
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading a user from a database...');
+        return {id: id, name: 'githubUser'};
+    }, 2000);
+}

fbc5e9576dc047b10f03a51aaeb99258245a7ed2
Jai Vasani <jsvasani@gmail.com>
2019-04-07 19:56:48 -0500

    Removed callbacks and added promises.

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,23 +1,26 @@
 console.log('Before');
-getUser(1, (user) => {
-    // Get repositories
-    getRepositories(user.githubUsername, (repositories) => {
-    console.log('Repos:', repositories);
-    });
-});
+
+getUser(1)
+    .then(user => getRepositories(user.githubUsername))
+    .then(repos => console.log('Repos:', repos))
+    .catch(err => console.log('Error:', err.message));
 
 console.log('After');
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database...');
-        callback({id: id, githubUsername: 'jaivasani'});
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({id: id, githubUsername: 'jaivasani'});
+        }, 2000);
+    });
 }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Using github api to read the repositories of the user', username);
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Using github api to read the repositories of the user', username);
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000); 
+    });
 }
